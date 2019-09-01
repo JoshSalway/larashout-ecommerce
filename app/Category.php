bb9bfa8441981bc364f1e962ff77b03ee736178f
= <josh.salway@gmail.com>
2019-09-01 20:27:57 +1000

    Part 10 WIP

---
+++ app/Category.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Category extends Model
+{
+    protected $table = 'categories';
+
+    protected $fillable = [
+        'name', 'slug', 'description', 'parent_id', 'featured', 'menu', 'image'
+    ];
+
+    protected $casts = [
+        'parent_id' => 'integer',
+        'featured' => 'boolean',
+        'menu' => 'boolean'
+    ];
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+
+    public function children()
+    {
+        return $this->hasMany(Category::class, 'parent_id');
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo(Category::class, 'parent_id');
+    }
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class, 'product_categories', 'category_id', 'product_id');
+    }
+}
