03074431bf0ba7e218ad56070c5b10092595b533
= <josh.salway@gmail.com>
2019-09-01 19:56:06 +1000

    Part 9 WIP

--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Setting;
 use Illuminate\Http\Request;
 use App\Http\Controllers\BaseController;
+use Illuminate\Http\UploadedFile;
 
 class SettingController extends BaseController
 {
@@ -15,6 +17,32 @@ class SettingController extends BaseController
 
     public function update(Request $request)
     {
+        if ($request->has('site_logo') && ($request->file('site_logo') instanceof UploadedFile)) {
 
+            if (config('settings.site_logo') != null) {
+                $this->deleteOne(config('settings.site_logo'));
+            }
+            $logo = $this->uploadOne($request->file('site_logo'), 'img');
+            Setting::set('site_logo', $logo);
+
+        } elseif ($request->has('site_favicon') && ($request->file('site_favicon') instanceof UploadedFile)) {
+
+            if (config('settings.site_favicon') != null) {
+                $this->deleteOne(config('settings.site_favicon'));
+            }
+            $favicon = $this->uploadOne($request - file('site_favicon'), 'img');
+            Setting::set('site_favicon', $favicon);
+
+        } else {
+
+            $keys = $request->except('_token');
+
+            foreach ($keys as $key => $value)
+            {
+                Setting::set($key, $value);
+            }
+
+        }
+        return $this->responseRedirectBack('Settings updated successfully.', 'success');
     }
 }
